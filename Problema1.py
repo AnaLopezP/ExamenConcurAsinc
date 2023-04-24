49b000f42039d0d53ae59db32d5c2fa50ccf90ca
Ana López <anamuyamiga@gmail.com>
2023-04-24 18:24:31 +0200

    sacrificad una cabra esta noche, hay luna llena

--- Problema1.py
+++ Problema1.py
@@ -1,3 +1,4 @@
+from queue import Queue 
 from multiprocessing import Pool
 
 '''Se parte de una cuenta con 100 euros y se pueden tener procesos que ingresen 100 euros, 50 o 20. También se pueden tener procesos que retiran 100, 50 o 20 euros euros. Se desean tener los siguientes procesos:
@@ -25,21 +26,25 @@ class Banco():
         self.dinero = 100
         #operacion = 1
 
-    def piscina(self, proc, func, dinero):
-        piscina = Pool(processes = proc)
-        piscina.map(func, dinero)
-        piscina.close()
-    
-
     def ingresar(self, cantidad):
         self.dinero = self.dinero + int(cantidad)
+        print(self.dinero)
         return self.dinero
     
-        
+        #self.operacion += 1
+    
     def retirar(self, cantidad):
-        self.dinero = self.dinero - int(cantidad)
-        return self.dinero
-    
+            '''cola.join()#Paramos la cola hasta que se hayan hecho todas las operaciones
+            cola.put(self.operacion)'''
+            self.dinero = self.dinero - int(cantidad)
+            return self.dinero
+            #self.operacion -= 1
+
+
+def piscina(proc, func, dinero):
+    piscina = Pool(processes = proc)
+    piscina.map(func, dinero)
+    piscina.close()
 
 
 if __name__ == '__main__':
@@ -55,17 +60,17 @@ if __name__ == '__main__':
     cuenta = Banco()
     print("Dinero inicial: " + str(cuenta.dinero))
 
-    cuenta.piscina(40, cuenta.ingresar, c1)
-    print(cuenta.dinero)
-    cuenta.piscina(20, cuenta.ingresar, c2)
-    print(cuenta.dinero)
-    cuenta.piscina(60, cuenta.ingresar, c3)
-    print(cuenta.dinero)
-    cuenta.piscina(40, cuenta.retirar, c1)
-    print(cuenta.dinero)
-    cuenta.piscina(20, cuenta.retirar, c2)
-    print(cuenta.dinero)
-    cuenta.piscina(60, cuenta.retirar, c3)
-    print(cuenta.dinero)
+    piscina(40, cuenta.ingresar, c1)
+    #print(cuenta.dinero)
+    piscina(20, cuenta.ingresar, c2)
+    #print(cuenta.dinero)
+    piscina(60, cuenta.ingresar, c3)
+    #print(cuenta.dinero)
+    piscina(40, cuenta.retirar, c1)
+    #print(cuenta.dinero)
+    piscina(20, cuenta.retirar, c2)
+    #print(cuenta.dinero)
+    piscina(60, cuenta.retirar, c3)
+    #print(cuenta.dinero)
 
     print("Dinero final: " + str(cuenta.dinero))
